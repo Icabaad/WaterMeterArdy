65c2c847543ee8112341f4ce61531fcc31a2d6bf
TheDanger <github@fallenreich.com>
2014-09-11 21:41:18 +1000

    Split out repository

---
+++ WaterFlowtestLCD.ino
@@ -0,0 +1,192 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13. 
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead(). 
+ 
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ * Note: because most Arduinos have a built-in LED attached 
+ to pin 13 on the board, the LED is optional.
+ 
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://arduino.cc/en/Tutorial/AnalogInput
+ 
+ */
+ #include <LiquidCrystal.h>
+ #include <Wire.h>
+ #define MAG_ADDR  0x0E //7-bit address for the MAG3110, doesn't change
+ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);  
+    Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  config();            // turn the MAG3110 on
+    lcd.begin(16, 2);
+        lcd.clear();
+}
+
+void loop() {
+   
+
+      lcd.write(Serial.read());  
+      
+        print_values();
+  delay(5);
+}
+
+void config(void)
+{
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x11);              // cntrl register2
+  Wire.write(0x80);              // send 0x80, enable auto resets
+  Wire.endTransmission();       // stop transmitting
+  
+  delay(15);
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x10);              // cntrl register1
+  Wire.write(1);                 // send 0x01, active mode
+  Wire.endTransmission();       // stop transmitting
+}
+
+void print_values(void)
+{
+  Serial.print("x=");
+        lcd.write("x="); 
+  Serial.print(readx()); 
+        lcd.write(readx()); 
+  Serial.print(",");  
+        lcd.write(","); 
+  Serial.print("y=");   
+       lcd.write("y=");  
+  Serial.print(ready());
+        lcd.write(ready()); 
+  Serial.print(",");  
+      lcd.write(",");   
+  Serial.print("z=");  
+      lcd.write("z=");   
+  Serial.println(readz()); 
+      lcd.write(readz());   
+}
+
+int readx(void)
+{
+  int xl, xh;  //define the MSB and LSB
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x01);              // x MSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    xh = Wire.read(); // receive the byte
+  }
+  
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x02);              // x LSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    xl = Wire.read(); // receive the byte
+  }
+  
+  int xout = (xl|(xh << 8)); //concatenate the MSB and LSB
+  return xout;
+}
+
+int ready(void)
+{
+  int yl, yh;  //define the MSB and LSB
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x03);              // y MSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    yh = Wire.read(); // receive the byte
+  }
+  
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x04);              // y LSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    yl = Wire.read(); // receive the byte
+  }
+  
+  int yout = (yl|(yh << 8)); //concatenate the MSB and LSB
+  return yout;
+}
+
+int readz(void)
+{
+  int zl, zh;  //define the MSB and LSB
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x05);              // z MSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    zh = Wire.read(); // receive the byte
+  }
+  
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
+  Wire.write(0x06);              // z LSB reg
+  Wire.endTransmission();       // stop transmitting
+ 
+  delayMicroseconds(2); //needs at least 1.3us free time between start and stop
+  
+  Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
+  while(Wire.available())    // slave may send less than requested
+  { 
+    zl = Wire.read(); // receive the byte
+  }
+  
+  int zout = (zl|(zh << 8)); //concatenate the MSB and LSB
+  return zout;
+}
