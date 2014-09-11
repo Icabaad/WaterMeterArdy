d4cef984b7f2af3b4f56a74cfc71fb35dc0236f4
TheDanger <github@fallenreich.com>
2014-09-11 23:36:06 +1000

    Added initial wave measurement

--- WaterFlowtestLCD2/WaterFlowtestLCD2.ino
+++ WaterFlowtestLCD2/WaterFlowtestLCD2.ino
@@ -7,18 +7,19 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
-
-  int newVal = readx(); //New Reading from MAX3110
+  int newVal = 0; //New Reading from MAX3110
   int prevVal = 0; //Previous Reading from MAX3110
   int maxVal = 0; //minumum magnetic field measured
   int minVal = 0; //minumum magnetic field measured
   int newRising = 0; //newVal rising? true or false?
   int oldRising = 0; //previous rising state
   int revs = 0; //revolutions of water meter totalled for interval time
-  long previousMillis = 0;
-  long interval = 60000; //Send data via xbee every 60 Seconds
-  const int revTick = 0.25; //For converting revolutions(revs) of meter to millilitres. You have to figure this out
+ long previousMillis = 0;
+ long interval = 60000; //Send data via xbee every 60 Seconds
+  int revTick = 0.25; //For converting revolutions(revs) of meter to millilitres. You have to figure this out
   int waterUse = 0;
+  
+  
 void setup() {
   // declare the ledPin as an OUTPUT:
 
@@ -26,13 +27,12 @@ void setup() {
   Serial.begin(9600);  // start serial for output
   config();            // turn the MAG3110 on
   lcd.begin(16, 2);
-
+  lcd.clear();
 }
 
 void loop() {
 
-
-
+  newVal = readx();
 
   if(newVal > maxVal) {
     maxVal = newVal;
@@ -46,6 +46,7 @@ void loop() {
   else if(newVal <= (minVal + 100)) {
     newRising = 0;
   } 
+  
   if((oldRising == 0) && (newRising == 1)) {
     revs ++;
     oldRising = newRising;
@@ -55,7 +56,8 @@ void loop() {
   if(currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;  
     waterUse = (revs * revTick);
-  }
+ }
+  
   print_values();
   delay(5);
 }
@@ -67,9 +69,9 @@ void config(void)
   Wire.write(0x11);              // cntrl register2
   Wire.write(0x80);              // send 0x80, enable auto resets
   Wire.endTransmission();       // stop transmitting
-
+  
   delay(15);
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x10);              // cntrl register1
   Wire.write(1);                 // send 0x01, active mode
@@ -103,33 +105,33 @@ void print_values(void)
 int readx(void)
 {
   int xl, xh;  //define the MSB and LSB
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x01);              // x MSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     xh = Wire.read(); // receive the byte
   }
-
+  
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x02);              // x LSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     xl = Wire.read(); // receive the byte
   }
-
+  
   int xout = (xl|(xh << 8)); //concatenate the MSB and LSB
   return xout;
 }
@@ -137,33 +139,33 @@ int readx(void)
 int ready(void)
 {
   int yl, yh;  //define the MSB and LSB
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x03);              // y MSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     yh = Wire.read(); // receive the byte
   }
-
+  
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x04);              // y LSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     yl = Wire.read(); // receive the byte
   }
-
+  
   int yout = (yl|(yh << 8)); //concatenate the MSB and LSB
   return yout;
 }
@@ -171,36 +173,33 @@ int ready(void)
 int readz(void)
 {
   int zl, zh;  //define the MSB and LSB
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x05);              // z MSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     zh = Wire.read(); // receive the byte
   }
-
+  
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.beginTransmission(MAG_ADDR); // transmit to device 0x0E
   Wire.write(0x06);              // z LSB reg
   Wire.endTransmission();       // stop transmitting
-
+ 
   delayMicroseconds(2); //needs at least 1.3us free time between start and stop
-
+  
   Wire.requestFrom(MAG_ADDR, 1); // request 1 byte
   while(Wire.available())    // slave may send less than requested
   { 
     zl = Wire.read(); // receive the byte
   }
-
+  
   int zout = (zl|(zh << 8)); //concatenate the MSB and LSB
   return zout;
 }
-
-
-
